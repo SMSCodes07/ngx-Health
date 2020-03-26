ca1cce336d7d786edeed155d3c5d261eb1df76b0
SMSCodes07 <48341791+SMSCodes07@users.noreply.github.com>
2020-03-25 20:44:26 -0400

    Home Commit
    En este commit se hicimos el home, en grupo. Fue divertido.

--- src/app/core/components/public/home/home.component.ts
+++ src/app/core/components/public/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  // Funcion para ir al login
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+  // Funcion para ir al login
+  // Funcion para ir al registro
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
+  // Funcion para ir al registro
 }
